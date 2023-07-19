46199fafa0f0e3c0446552e4169b299dabaa2a54
axlrose0023 <sloboda282@gmail.com>
2023-07-19 11:41:40 +0300

    github pages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 function Header() {
     return <nav>
         <div className="nav-wrapper">
-            <a href="#" className="brand-logo">React Shop</a>
+            <a href="/" className="brand-logo">React Shop</a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="#">Repo</a></li>
+                <li><a
+                    href="https://github.com/axlrose023/react_shop/deployments/activity_log?environment=github-pages"
+                    rel='noreferrer'>Repo</a>
+                </li>
             </ul>
         </div>
     </nav>
